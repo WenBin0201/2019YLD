13bf40f5745072b746cdc755ebb08c08ff57b699
Yangxian <570486455@qq.com>
2019-01-12 00:05:49 +0800

    添加忘记密码界面

--- app/navigator/app_navigator.js
+++ app/navigator/app_navigator.js
@@ -12,6 +12,7 @@ import Detail from '../page/test/Detail'
 import PgSettle from '../page/home/receipts/PgSettle' //结算页面
 import TakeBillBoxContainer from '../component/takebill/TakeBillBoxContainer'
 import ShiftRecord from '../page/login/ShiftRecord'
+import PasswordRetrieve from '../page/login/PasswordRetrieve'
 
 /**
  * 登录模块
@@ -20,13 +21,14 @@ const LoginStack = createStackNavigator(
     {
         Login: Login,
         ShiftRecord: ShiftRecord,
+        PasswordRetrieve
     },
     {
         navigationOptions: {},
         headerMode: 'none',
         transitionConfig: TransitionConfiguration
     }
-);
+)
 
 /**
  * 主页模块
@@ -37,33 +39,31 @@ const HomeStack = createStackNavigator(
         PgSettle: PgSettle,
         Detail: Detail,
         Setting: Setting,
-        TakeBillBoxContainer: TakeBillBoxContainer,
+        TakeBillBoxContainer: TakeBillBoxContainer
     },
     {
         navigationOptions: {},
         headerMode: 'none',
         transitionConfig: TransitionConfiguration
     }
-);
-
+)
 
 export const AppNavigator = createSwitchNavigator({
     LoginStack: LoginStack,
     HomeStack: HomeStack
 })
 
-
 /**
  * 页面统一跳转动画
  * 默认右进左出
  * 从底部弹入（this.props.navigate('ScreenKey', { transition: 'forVertical' });）；
  */
-const TransitionConfiguration = () => ({
+const TransitionConfiguration = () => ( {
     screenInterpolator: (sceneProps) => {
-        const {scene} = sceneProps;
-        const {route} = scene;
-        const params = route.params || {};
-        const transition = params.transition || 'forHorizontal';
-        return StackViewStyleInterpolator[transition](sceneProps);
+        const { scene } = sceneProps
+        const { route } = scene
+        const params = route.params || {}
+        const transition = params.transition || 'forHorizontal'
+        return StackViewStyleInterpolator[ transition ](sceneProps)
     }
-});
+} )
